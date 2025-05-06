4895023b7b9fbd138490f74d39f32f249448d351
Chinae <Chinaenwankwo@gmail.com>
2025-05-06 14:33:58 -0500

    yes

--- plant_bash.sh
+++ plant_bash.sh
@@ -2,4 +2,22 @@
 echo "Hello welcome"
 echo "What is your name"
 read name
-echo $name
+$name
+echo "Welcome $name to our planting simulator"
+sleep 5
+exit 0
+
+echo "Time to bigin our planting simulator"
+response = input("would you like to plant a new seed, yes or no")
+
+if response = "yes" then;
+echo (Let's begin our planting simulator)-
+
+fi
+
+if response = "no" echo (Good bye have a great $name day)
+exit 
+
+if yes echo "$name you have dug a hole and planted a small seed of the size of 3"
+if no echo "Good bye have a great day $name"
+
